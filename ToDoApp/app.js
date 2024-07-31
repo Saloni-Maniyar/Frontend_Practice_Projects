fd4adc031e7c6c7793f4c40cf14c3432f0f1f8a9
Saloni Maniyar <salonimaniyar16@gmail.com>
2024-08-01 00:27:55 +0530

    add button function added

--- ToDoApp/app.js
+++ ToDoApp/app.js
@@ -25,3 +25,21 @@
 //     }
 //     req=prompt("Enter your another request now:");
 // }
+
+const inputBox=document.getElementById("input-box");
+const listContainer=document.querySelector(".list-container");
+function addTask(){
+    if(inputBox.value===''){
+        alert("You must write something!!");
+    }else{
+        let li=document.createElement("li");
+        li.innerHTML=inputBox.value;
+        listContainer.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+       
+    }
+    inputBox.value='';
+}
+
