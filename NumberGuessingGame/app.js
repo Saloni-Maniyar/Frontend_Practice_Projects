16b567734f5a17e00d01a1ad480900f2f8a9fb17
Saloni Maniyar <salonimaniyar16@gmail.com>
2024-08-08 16:34:18 +0530

    updated

--- NumberGuessingGame/app.js
+++ NumberGuessingGame/app.js
@@ -1,56 +1,55 @@
-let main_content=document.querySelector("#main-content");
-let rangeContainer=document.querySelector(".GetRange");
-let rangeInput=document.getElementById("maxRangeBox");
-let gameContainer=document.querySelector(".game-container");
-let guessInput=document.getElementById("guess");
 let gameSection=document.querySelector("section");
-let allGameDiv=document.querySelectorAll(".game-div");
+let gameContainer=document.querySelector(".game");
+let divs=gameContainer.querySelectorAll("div");
+let rangeContainer=document.querySelector(".GetRange");
+let guessContainer=document.querySelector(".guess-container");
+let rangeInput=document.getElementById("maxRangeBox");
+let guessInput=document.getElementById("guess");
 let quitDiv=document.querySelector(".quitGame");
-let max,guess,random;
+let max,guessNum,random;
 let msgDiv;
-
+guessContainer.style.display="none";
 console.log("rangeContaier=",rangeContainer);
 console.log("rangeInput=",rangeInput);
-console.log("game container=",gameContainer);
+console.log("game container=",guessContainer);
 rangeInput.addEventListener("keydown",function(e){
     if(e.key=="Enter"){
         console.log("Enter pressed");
         max=parseInt(rangeInput.value);
         console.log("max=",max);
         rangeContainer.style.display="none";
-        gameContainer.style.display="block";
+        guessContainer.style.display="block";
         rangeInput.value='';
     }
 });
 guessInput.addEventListener("keydown",function(e){
     if(e.key=="Enter"){
         console.log("guess enter pressed");
-        guess=parseInt(guessInput.value);
-        console.log("guess="+guess);
+        guessNum=parseInt(guessInput.value);
+        console.log("guess="+guessNum);
         random=Math.floor(Math.random()*max)+1;
         console.log("random=",random);
         if(msgDiv){
             msgDiv.remove();
         }
-        if(random===guess){
+        if(random===guessNum){
             correctGuess();
         }else{
             msgDiv=document.createElement("h4");
             msgDiv.id='msg';
-            // msgDiv.classList.add("wrong-guess-Msg");
-            msgDiv.innerHTML="Wrong msg,Please Try again!!";
-            gameSection.appendChild(msgDiv);
+            msgDiv.innerHTML="Wrong guess,Please Try again!!";
+            gameContainer.appendChild(msgDiv);
         }
         guessInput.value='';
     }
 });
 function correctGuess(){
-     gameSection.forEach(div => div.style.display = "none"); 
-    // gameSection.style.display="none";
+    console.log("inside correctGuess");
+    divs.forEach(div => div.style.display = "none"); 
     let continueDiv=document.createElement("div");
     continueDiv.classList.add("continue-div");
     let congo_msg=document.createElement("h1");
-    congo_msg.id='msg';
+    congo_msg.id='Congomsg';
     congo_msg.innerHTML="Congratulations!! You guess correct number."
     continueDiv.appendChild(congo_msg);
     
@@ -58,13 +57,17 @@ function correctGuess(){
     continueButn.id='con-btn';
     continueButn.classList.add('btn');
     continueButn.innerHTML="continue";
-    // continueDiv.style.display="block";
-    continueDiv.appendChild(continueButn);
-    main_content.appendChild(continueDiv);
-    
     quitDiv.style.display="inline";
-    continueDiv.appendChild(quitDiv);
-    // gameSection.appendChild(continueDiv);
+    let btnDiv=document.createElement("div");
+    btnDiv.classList.add("btnDiv");
+    btnDiv.appendChild(continueButn);
+    btnDiv.appendChild(quitDiv);
+    continueDiv.appendChild(btnDiv);
+    gameContainer.style.border="none";
+    gameContainer.style.background="transparent";
+    gameContainer.style.width="fit-content";
+
+    gameContainer.appendChild(continueDiv);
     
 }
 
